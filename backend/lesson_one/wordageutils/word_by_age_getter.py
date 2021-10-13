baf4510555f323e2cd416d7ce394d1b4e67d3904
thepicture <fuzzyeastern@protonmail.com>
2021-10-13 14:19:18 +0100

    Implemented word_by_age_getter.py

---
+++ backend/lesson_one/wordageutils/word_by_age_getter.py
@@ -0,0 +1,12 @@
+def get_word_by_age(age):
+    """Get appropriate word for the given age."""
+    last_two_digits = age[-2:]
+    age = int(last_two_digits)
+
+    if age in range(10, 20) or age % 5 == 0:
+        return 'лет'
+    if age % 10 == 1:
+        return 'год'
+    if age % 10 in range(2, 5):
+        return 'года'
+    return 'лет'
