64b8ea63fa8d9dd04a1a428292a804b3bb26afc3
thepicture <fuzzyeastern@protonmail.com>
2021-10-13 15:17:50 +0100

    Wrote test for NumberGuesser

---
+++ backend/lesson_one/numberguesser/test_number_guesser.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from backend.lesson_one.numberguesser.number_guesser import (
+    NumberGuesser
+)
+
+
+class TestNumberGuesser(TestCase):
+    def test_get_mean_number(self):
+        """Test for correct guessing of the 73 number in [log2(100)] attempts."""
+
+        # Arrange.
+        guesser = NumberGuesser(0, 100)
+        guesser.ask()
+        guesser.say_greater()
+        guesser.say_less()
+        guesser.say_greater()
+        guesser.say_greater()
+        guesser.say_greater()
+        guesser.say_less()
+        expected = 73
+        # Act.
+        actual = guesser.get_mean_number()
+        # Assert.
+        TestCase.assertEqual(self, expected, actual)
