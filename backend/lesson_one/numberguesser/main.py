dfd17c872e4e0883e2374ee097f68f22d77c46ca
thepicture <fuzzyeastern@protonmail.com>
2021-10-13 15:27:15 +0100

    Remove is_done from NumberGuesser, implemented main for number_guesser.py

--- backend/lesson_one/numberguesser/main.py
+++ backend/lesson_one/numberguesser/main.py
@@ -1,1 +1,28 @@
+from backend.lesson_one.numberguesser.number_guesser import (
+    NumberGuesser
+)
+
 if __name__ == '__main__':
+    inf = 0
+    sup = 100
+
+    guesser = NumberGuesser(inf, sup)
+
+    while True:
+        guesser.ask()
+
+        answer = input()
+
+        if answer == 'Больше':
+            guesser.say_greater()
+            continue
+        elif answer == 'Меньше':
+            guesser.say_less()
+            continue
+        elif answer == 'Верно':
+            guesser.say_equal()
+            break
+        else:
+            print('Unknown command. '
+                  'Please, use "Больше" or "Меньше" or "Верно" '
+                  'to answer.')
--- backend/lesson_one/numberguesser/number_guesser.py
+++ backend/lesson_one/numberguesser/number_guesser.py
@@ -7,7 +7,6 @@ class NumberGuesser:
         """Init with the minimum and maximum values"""
         self.inf = inf
         self.sup = sup
-        self.is_done = False
 
     def say_greater(self):
         """Notifies the guesser that the number is greater
@@ -15,7 +14,6 @@ class NumberGuesser:
 
         """
         self.inf = self.get_mean_number()
-        self.ask()
 
     def say_less(self):
         """Notifies the guesser that the number is less
@@ -23,7 +21,6 @@ class NumberGuesser:
 
                """
         self.sup = self.get_mean_number()
-        self.ask()
 
     def say_equal(self):
         """Notifies the guesser that the number does equal
@@ -32,18 +29,11 @@ class NumberGuesser:
                """
         print(f'The number is {self.get_mean_number()}')
 
-        self.is_done = True
-
     def ask(self):
         """Invokes the guesser to offer a number."""
-        print(self.inf, self.sup)
         print(f'Is the number greater or equals to '
               f'{self.get_mean_number()}?')
 
     def get_mean_number(self):
         """Return the mean of the minimum and maximum."""
         return math.ceil((self.inf + self.sup) / 2)
-
-    def is_guessed(self):
-        """Check if the number was guessed."""
-        return self.is_done
