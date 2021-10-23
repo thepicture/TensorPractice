3d68bd2ec7669151f95a4fe3911c2f63caff565c
thepicture <fuzzyeastern@protonmail.com>
2021-10-23 17:41:24 +0100

    Add logger decorator

--- backend/lesson_two/main.py
+++ backend/lesson_two/main.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from backend.lesson_two.fieldutils import neighbor_counter
 from backend.lesson_two.fieldutils.field import Field
 from backend.lesson_two.iofieldutils import field_saver
@@ -14,47 +16,68 @@ assert MAX_WIDTH == MAX_HEIGHT
 
 
 def main():
-    """The game "Life" init.
+    """The game "Life" main start point.
     """
-    field_list, generations_count = parse_file('input.txt')
+    input_file = 'input.txt'
+
+    field_list, generations_count = parse_file(input_file)
     field = Field(field_list)
+
     if field.get_width() >= 20:
         print(MESSAGE_TEMPLATE % ('column',
                                   MAX_WIDTH,
-                                  field.get_width()))
+                                  field.get_width(),
+                                  )
+              )
         return
     if field.get_height() >= 20:
         print(MESSAGE_TEMPLATE % ('row',
                                   MAX_HEIGHT,
-                                  field.get_height()))
+                                  field.get_height()),
+              )
         return
 
     for generation in range(generations_count):
         calculate_generation(field, generation)
 
 
+def generation_logger(func):
+    """Logs generations in after and
+    before states in the console.
+    """
+
+    @wraps(func)
+    def wrapper(field, generation, **kwargs):
+        print('Generation %s:' % (generation + 1))
+        print('Before:')
+        field.print()
+        func(field, generation, **kwargs)
+        print('After: ')
+        field.print()
+
+    return wrapper
+
+
+@generation_logger
 def calculate_generation(field, generation):
-    """Calculate the system state
+    """Calculates the current system state
     for the given step.
     """
     generation_history = dict()
-    print('Generation %s:' % (generation + 1))
-    print('Before:')
     field_saver.save_field(field)
-    field.print()
     for x in range(field.get_width()):
         loop_for_y_and_update(field, generation_history, x)
     for history_x, history_y in generation_history:
-        field.set(history_x, history_y,
-                  generation_history[history_x,
-                                     history_y]
+        field.set(history_x,
+                  history_y,
+                  generation_history[
+                      history_x, history_y
+                  ],
                   )
-    print('After: ')
-    field.print()
 
 
 def loop_for_y_and_update(field, generation_history, x):
-    """Loops the generation state and
+    """Loops the generation state for y-axis and
     updates life cycle.
     """
     for y in range(field.get_height()):
@@ -68,16 +91,16 @@ def update_field(field, generation_history, x, y):
     """
     count_of_neighbors = neighbor_counter.get_count_of_neighbors(
         field,
-        x, y
+        x, y,
     )
     generation_history.update(check_generation_conditions(
         field, x,
-        y, count_of_neighbors
+        y, count_of_neighbors,
     ))
 
 
 def check_generation_conditions(field, x,
-                                y, count_of_neighbors
+                                y, count_of_neighbors,
                                 ):
     """Checks if the given conditions are right:
 
@@ -85,12 +108,12 @@ def check_generation_conditions(field, x,
     then give a life to the cell.
 
     Else if the cell contains from two to three neighbors,
-    then pass, otherwise the cell is death."""
+    then pass, otherwise the cell will be dead."""
     generation_history = dict()
 
     if not field.get(x, y):
         update_if_3_neighbors(count_of_neighbors, generation_history,
-                              x, y
+                              x, y,
                               )
     elif count_of_neighbors in range(2, 4):
         generation_history[x, y] = 1
@@ -100,7 +123,9 @@ def check_generation_conditions(field, x,
     return generation_history
 
 
-def update_if_3_neighbors(count_of_neighbors, generation_history, x, y):
+def update_if_3_neighbors(count_of_neighbors, generation_history,
+                          x, y,
+                          ):
     """Writes the history if
     the cell has exactly three neighbors.
     """
