027dcab81a696e88187ef985502ad11140f350cc
thepicture <fuzzyeastern@protonmail.com>
2021-10-24 11:46:16 +0100

    Implemented task21

--- frontend/lesson_2/task2.js
+++ frontend/lesson_2/task2.js
@@ -1,6 +1,29 @@
-
 function task21(arr) {
-    return arr;
+    return arr
+        .map(function (val) {
+            return {
+                year: val.year,
+                month: val.month
+            };
+        })
+        .reduce(function (acc, val) {
+            const payment = acc.find(function (el) {
+                return el.month === val.month;
+            });
+
+            if (!payment) {
+                val.totalOps = 1
+                acc.push(val);
+            } else {
+                payment.totalOps++;
+            }
+
+            return acc
+                .sort(function (prev, next) {
+                    return next.totalOps - prev.totalOps
+                });
+        }, [])
+        .slice(0, 3);
 }
 
 function task22(year, month, arr) {
