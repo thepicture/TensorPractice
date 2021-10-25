1a70cacc240cb95acf5c0bcf04f5bbe48cd59fa0
thepicture <fuzzyeastern@protonmail.com>
2021-10-25 11:19:50 +0100

    Fixed comment at the fieldprinter.py

--- backend/lesson_two/fieldutils/fieldprinter.py
+++ backend/lesson_two/fieldutils/fieldprinter.py
@@ -1,7 +1,7 @@
 class FieldPrinter:
     @staticmethod
     def print(field):
-        """Prints this field in a readable form."""
+        """Prints the given field in a readable form."""
         for y in range(field.get_height()):
             for x in range(field.get_width()):
                 print(field.get(x, y), end=' ')
